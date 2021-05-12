6e098c9ed7f08e5ba2f17a89fac0166a71284c63
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-05-12 22:10:58 +0900

    Refactor : Redux Thunk

--- redux-middleware/src/lib/asyncUtils.js
+++ redux-middleware/src/lib/asyncUtils.js
@@ -1,3 +1,52 @@
+export const createPromiseThunk = (type, promiseCreator) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+  return (param) => async (dispatch) => {
+    dispatch({ type });
+    try {
+      const payload = await promiseCreator(param);
+      dispatch({
+        type: SUCCESS,
+        payload,
+      });
+    } catch (e) {
+      dispatch({
+        type: ERROR,
+        payload: e,
+        error: true,
+      });
+    }
+  };
+};
+
+export const handleAsyncActions = (type, key) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+  return (state, action) => {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: reducerUtils.loading(),
+        };
+
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: reducerUtils.success(action.payload),
+        };
+
+      case ERROR:
+        return {
+          ...state,
+          [key]: reducerUtils.error(action.payload),
+        };
+
+      default:
+        return state;
+    }
+  };
+};
+
 export const reducerUtils = {
   initial: (data = null) => ({
     data,
